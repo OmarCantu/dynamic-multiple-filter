182185a561b976e61610d845ba3a04554bb4f2a9
Omar Cantú <omar.a.cm@hotmail.com>
2019-03-27 23:35:31 +1100

    Add configuration for jest unit testing

---
+++ src/components/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { shallow } from 'enzyme';
+
+// import App from './App';
+// import store from '../store';
+
+// it('renders without crashing', () => {
+//   shallow(<Provider store={store}><App /></Provider>);
+// });
+
+test('Some test', () => {
+  expect(true).toBeTruthy();
+})
+
